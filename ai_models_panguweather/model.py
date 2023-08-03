b1bb8bbf4424c5742a340382a3f069f0455cf393
Mikko Partio <mikko.partio@fmi.fi>
2023-08-03 08:05:37 +0300

    Allow running in HRES lead time configuration

--- ai_models_panguweather/model.py
+++ ai_models_panguweather/model.py
@@ -21,7 +21,12 @@ class PanguWeather(Model):
     download_url = (
         "https://get.ecmwf.int/repository/test-data/ai-models/pangu-weather/{file}"
     )
-    download_files = ["pangu_weather_24.onnx", "pangu_weather_6.onnx"]
+    download_files = [
+        "pangu_weather_24.onnx",
+        "pangu_weather_6.onnx",
+        "pangu_weather_3.onnx",
+        "pangu_weather_1.onnx",
+    ]
 
     # Input
     area = [90, 0, -90, 360]
@@ -55,9 +60,6 @@ class PanguWeather(Model):
 
         fields_sfc_numpy = fields_sfc.to_numpy(dtype=np.float32)
 
-        input = fields_pl_numpy
-        input_surface = fields_sfc_numpy
-
         options = ort.SessionOptions()
         options.enable_cpu_mem_arena = False
         options.enable_mem_pattern = False
@@ -66,11 +68,15 @@ class PanguWeather(Model):
 
         pangu_weather_24 = os.path.join(self.assets, "pangu_weather_24.onnx")
         pangu_weather_6 = os.path.join(self.assets, "pangu_weather_6.onnx")
+        pangu_weather_3 = os.path.join(self.assets, "pangu_weather_3.onnx")
+        pangu_weather_1 = os.path.join(self.assets, "pangu_weather_1.onnx")
 
         # That will trigger a FileNotFoundError
 
         os.stat(pangu_weather_24)
         os.stat(pangu_weather_6)
+        os.stat(pangu_weather_3)
+        os.stat(pangu_weather_1)
 
         with self.timer(f"Loading {pangu_weather_24}"):
             ort_session_24 = ort.InferenceSession(
@@ -86,40 +92,106 @@ class PanguWeather(Model):
                 providers=self.providers,
             )
 
-        input_24, input_surface_24 = input, input_surface
+        with self.timer(f"Loading {pangu_weather_3}"):
+            ort_session_3 = ort.InferenceSession(
+                pangu_weather_3,
+                sess_options=options,
+                providers=self.providers,
+            )
 
-        with self.stepper(6) as stepper:
-            for i in range(self.lead_time // 6):
-                step = (i + 1) * 6
+        with self.timer(f"Loading {pangu_weather_1}"):
+            ort_session_1 = ort.InferenceSession(
+                pangu_weather_1,
+                sess_options=options,
+                providers=self.providers,
+            )
 
-                if (i + 1) % 4 == 0:
-                    output, output_surface = ort_session_24.run(
-                        None,
-                        {
-                            "input": input_24,
-                            "input_surface": input_surface_24,
-                        },
-                    )
-                    input_24, input_surface_24 = output, output_surface
-                else:
-                    output, output_surface = ort_session_6.run(
-                        None,
-                        {
-                            "input": input,
-                            "input_surface": input_surface,
-                        },
-                    )
-                input, input_surface = output, output_surface
+        input_24, input_surface_24 = fields_pl_numpy, fields_sfc_numpy
+        input_6, input_surface_6 = fields_pl_numpy, fields_sfc_numpy
+        input_3, input_surface_3 = fields_pl_numpy, fields_sfc_numpy
+        input_1, input_surface_1 = fields_pl_numpy, fields_sfc_numpy
 
-                # Save the results
+        # L: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33
+        # M: 1,1,3,1,1,6,1,1,3, 1, 1, 6, 1, 1, 3, 1, 1, 6, 1, 1, 3, 1, 1,24, 1, 1, 3, 1, 1, 6, 1, 1, 3
 
-                pl_data = output.reshape((-1, 721, 1440))
+        def run_inference(step):
+            nonlocal input_24, input_surface_24, input_6, input_surface_6, input_3, input_surface_3, input_1, input_surface_1
 
-                for data, f in zip(pl_data, fields_pl):
-                    self.write(data, template=f, step=step)
+            if step % 24 == 0:
+                output, output_surface = ort_session_24.run(
+                    None,
+                    {
+                        "input": input_24,
+                        "input_surface": input_surface_24,
+                    },
+                )
+                input_24, input_surface_24 = output, output_surface
+                input_6, input_surface_6 = output, output_surface
+                input_3, input_surface_3 = output, output_surface
+                input_1, input_surface_1 = output, output_surface
 
-                sfc_data = output_surface.reshape((-1, 721, 1440))
-                for data, f in zip(sfc_data, fields_sfc):
-                    self.write(data, template=f, step=step)
+            elif step % 6 == 0:
+                output, output_surface = ort_session_6.run(
+                    None,
+                    {
+                        "input": input_6,
+                        "input_surface": input_surface_6,
+                    },
+                )
+                input_6, input_surface_6 = output, output_surface
+                input_3, input_surface_3 = output, output_surface
+                input_1, input_surface_1 = output, output_surface
 
-                stepper(i, step)
+            elif step % 3 == 0:
+                output, output_surface = ort_session_3.run(
+                    None,
+                    {
+                        "input": input_3,
+                        "input_surface": input_surface_3,
+                    },
+                )
+                input_3, input_surface_3 = output, output_surface
+                input_1, input_surface_1 = output, output_surface
+
+            else:
+                output, output_surface = ort_session_1.run(
+                    None,
+                    {
+                        "input": input_1,
+                        "input_surface": input_surface_1,
+                    },
+                )
+                input_1, input_surface_1 = output, output_surface
+
+            return output, output_surface
+
+        def save_results(output, output_surface):
+            pl_data = output.reshape((-1, 721, 1440))
+
+            for data, f in zip(pl_data, fields_pl):
+                self.write(data, template=f, step=step)
+
+            sfc_data = output_surface.reshape((-1, 721, 1440))
+            for data, f in zip(sfc_data, fields_sfc):
+                self.write(data, template=f, step=step)
+
+        if self.lead_time_configuration == "HRES":
+            self.lead_time = 90 + 18 + 16
+            with self.stepper(1) as stepper:
+                num = 0
+                for step in (
+                    list(range(1, 91))
+                    + list(range(93, 147, 3))
+                    + list(range(144, 246, 6))
+                ):
+                    output, output_surface = run_inference(step)
+                    save_results(output, output_surface)
+                    stepper(num, step)
+                    num += 1
+        else:
+            with self.stepper(6) as stepper:
+                for i in range(self.lead_time // 6):
+                    step = (i + 1) * 6
+                    output, output_surface = run_inference(step)
+                    save_results(output, output_surface)
+                    stepper(num, step)
